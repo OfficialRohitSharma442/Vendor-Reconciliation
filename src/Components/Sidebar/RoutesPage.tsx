c75b76aff10b2c524d26ea0dc6f2ad990c731dcd
Rohitash Sharma <rohitash.s@cubiclogics.com>
2023-11-28 00:16:01 +0530

    my report card compleated

--- src/Components/Sidebar/RoutesPage.tsx
+++ src/Components/Sidebar/RoutesPage.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom"
 import Adminmain from "../Admin/adminmain"
 import Vendor from "../Vendor/vendor"
+import Reports from "../Reports/Reports"
 
 
 const RoutesPage = () => {
@@ -9,7 +10,7 @@ const RoutesPage = () => {
             <Route path="/" element={<div>home page</div>} ></Route>
             <Route path="/Import" element={<div>Import page</div>} ></Route>
             <Route path="/VenderPage" element={<Vendor />} ></Route>
-            <Route path="/Reports" element={<div>Report page</div>} ></Route>
+            <Route path="/Reports" element={<Reports />} ></Route>
             <Route path="/Settings" element={<Adminmain />} ></Route>
             <Route path="/admin" element={<div>Super  Admin page</div>} ></Route>
         </Routes>
