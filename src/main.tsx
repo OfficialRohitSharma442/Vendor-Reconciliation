99428590a0b2a9169b1e881ece4ada01d169025b
Rohitash Sharma <rohitash.s@cubiclogics.com>
2023-12-15 00:52:36 +0530

    Rohit cahges

--- src/main.tsx
+++ src/main.tsx
@@ -5,15 +5,15 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom';
 import { ConfigProvider, theme } from 'antd';
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  // <ConfigProvider theme={{
-  //   token: {
-  //     colorBgLayout: "rgba(0, 0, 0, 0.02)"
-  //   }
-  // }}>
-
-
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-  // </ConfigProvider>
+  <ConfigProvider theme={{
+    token: {
+      colorPrimary: '#00b96b',
+      borderRadius: 2,
+      colorBgSpotlight: "red"
+    },
+  }}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ConfigProvider>
 )
