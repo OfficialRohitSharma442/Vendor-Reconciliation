9fb14530054409d1ca1f6b6e7d35c79c17022bbe
Rohitash Sharma <rohitash.s@cubiclogics.com>
2023-12-20 22:08:05 +0530

    chages icone

--- src/Components/Sidebar/Sidebarheader.tsx
+++ src/Components/Sidebar/Sidebarheader.tsx
@@ -1,6 +1,6 @@
 
 import { Button, Space } from 'antd';
-import { MenuFoldOutlined, ArrowLeftOutlined, ArrowRightOutlined, PoweroffOutlined, LogoutOutlined, CloseOutlined } from '@ant-design/icons';
+import { MenuOutlined, ArrowLeftOutlined, ArrowRightOutlined, PoweroffOutlined, LogoutOutlined, CloseOutlined } from '@ant-design/icons';
 import Typography from 'antd/es/typography/Typography';
 import myImage from "./../../assets/vendor-reconciliation-high-resolution-logo-black-transparent.png";
 
@@ -10,13 +10,10 @@ const SidebarHeader = ({ collapsed, setCollapsed }: any) => (
             {<div style={{ padding: "10px", textAlign: "center" }}>
                 {collapsed ?
                     <div className='expend_icons_sidebar'>
-
-                        <MenuFoldOutlined style={{ fontSize: '17px' }} onClick={() => setCollapsed(!collapsed)} />
+                        <MenuOutlined style={{ fontSize: '17px' }} onClick={() => setCollapsed(!collapsed)} />
                     </div>
-
                     :
                     <div className='hamburger_logo_withButton'>
-
                         <img src={myImage} style={{ width: "145px", height: "45px" }}></img>
                         <CloseOutlined className='closeIcone' onClick={() => setCollapsed(!collapsed)} style={{ fontSize: '17px' }} />
                     </div>
